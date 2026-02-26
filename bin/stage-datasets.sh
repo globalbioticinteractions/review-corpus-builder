230413aed103c8cc5b7743a7948c7e0d81b453b9
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-02-26 14:33:13 -0600

    add some debug flags

--- bin/stage-datasets.sh
+++ bin/stage-datasets.sh
@@ -12,4 +12,6 @@
 #   Elton, Nomer, & Preston. (2026). Versioned Archive and Review of Biotic Interactions and Taxon Names Found within globalbioticinteractions/maarjam hash://md5/17cb1afe09fab2499edfc1636640c7c6. Zenodo. https://doi.org/10.5281/zenodo.18792289
 #
 
+set -xe
+
 xargs -I{} preston ls --anchor {} --remote https://zenodo.org --algo md5
